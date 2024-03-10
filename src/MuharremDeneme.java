14bfc288379aa0b54eb4a46363f25e45c6c8190e
muharremyurtsever <muharremyurtsever@gmail.com>
2024-03-10 19:29:53 +0000

    deneme 2

--- src/MuharremDeneme.java
+++ src/MuharremDeneme.java
@@ -1,5 +1,12 @@
 public class MuharremDeneme {
     public static void main(String[] args) {
+
+        int ilk = 12;
+        int iki = 23;
+
+        System.out.println("Sonuç = "+ilk+iki);
+
         System.out.println("for Muharrem Pull Request Control");
+
     }
 }
